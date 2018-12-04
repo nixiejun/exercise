428431268af41e5e1447a003020e8ace06553fa6
nixiejun <nixiejun@163.com>
2018-12-04 19:08:13 +0800

    PartIII Distributing MR

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,12 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
+
 
 // schedule starts and waits for all tasks in the given phase (Map or Reduce).
 func (mr *Master) schedule(phase jobPhase) {
@@ -24,5 +30,41 @@ func (mr *Master) schedule(phase jobPhase) {
 	//
 	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
 	//
+
+	var waitgroup sync.WaitGroup
+	debug("ntasks: %v",ntasks)
+	debug("file size %v", len(mr.files))
+	for i := 0; i< ntasks;i++{
+		waitgroup.Add(1)
+		go func(taskNum int, nios int, phase jobPhase){
+			defer waitgroup.Done()
+			for {
+				worker := <-mr.registerChannel
+				var args DoTaskArgs
+				args.JobName = mr.jobName
+
+				args.File = mr.files[taskNum]
+
+				args.TaskNumber = taskNum
+				args.Phase = phase
+				args.NumOtherPhase = nios
+				ok := call(worker, "Worker.DoTask", &args, nil)
+				if ok != true {
+					log.Fatal("Remote call doTask i worker failed")
+				}
+				if ok {
+					go func() {
+						mr.registerChannel <- worker
+					}()
+					break
+				}
+			}
+		}(i, nios, phase)
+	}
+
+	// xni;12/01/2018
+	// Make main thread waiting rather than exiting.
+	waitgroup.Wait()
+
 	fmt.Printf("Schedule: %v phase done\n", phase)
 }
